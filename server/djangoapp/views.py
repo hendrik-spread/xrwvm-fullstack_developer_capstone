a6b4cbcb3c4972ead960059bee168390ab962ed3
Hendrik <hendrik@spread.ai>
2025-10-25 06:47:05 -0400

    Added Logout functionality

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -39,8 +39,11 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+@csrf_exempt
+def logout_request(request):
+    logout(request) # Terminate user session
+    data = {"userName":""} # Return empty username
+    return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
